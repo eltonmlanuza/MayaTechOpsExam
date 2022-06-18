4c95a0dc92e3ab17da00a064713fb719b62e51fd
Elton Mark Lanuza <96604999+eltonmlanuza@users.noreply.github.com>
2022-06-18 14:27:04 +0800

    Completed Conditions

--- SEO exercise 1-2/memory_check.sh
+++ SEO exercise 1-2/memory_check.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-usage() { echo "Enter required arguments: $0 [-c <45|90>] [-w <string>] [-e <email>]" 1>&2; exit 1; }
+usage() { echo "Enter required arguments: $0 [-c <critical_threshold>] [-w <warning_threshold>] [-e <email>]" 1>&2; exit 1; }
 
 while getopts ":c:w:e:" o; do
     case "${o}" in
@@ -37,8 +37,18 @@ echo $USED_MEMORY
 PER_MEMORY=$( free | grep Mem: | awk '{ print ($3/$2)*100 }' )
 echo $PER_MEMORY
 
-if (( $(echo "$c > $PER_MEMORY" |bc -l) )); then
-        echo "Below Threshold"
+if (($c > $w)); then
+        if (($(echo "$w > $PER_MEMORY" |bc -l))); then
+                echo "0, used memory is less than warning threshold"
+
+        elif (($(echo "$w <= $PER_MEMORY" |bc -l) && $(echo "$c > $PER_MEMORY" |bc -l))); then
+                echo "1, used memory greater than or equal to warning but less than the critical threshold"
+
+        else
+                echo "2, used memory is greater than or equal to threshold"
+                echo "top 10 processes"
+        fi
+
 else
-        echo "Above Threshold"
+        echo "Critical Threshold must be greater than warning threshold"
 fi
